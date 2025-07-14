f723dd48af10ee7cb75bc78aa597e4a3286bb16a
Bao Ho Gia <54936754+BaoHG1508@users.noreply.github.com>
2025-07-14 11:25:59 +0700

    chore: add header template

---
+++ extensions/packages/nextjs/app/components/Header.tsx.template.mjs
@@ -0,0 +1,8 @@
+export const preContent = `import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";`;
+export const extraMenuLinksObjects = [
+  {
+    label: "Auco",
+    href: "/auco-greetings",
+    icon: '$$<MagnifyingGlassIcon className="h-4 w-4" />$$',
+  },
+];
