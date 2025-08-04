2c288985cfe661be969741a4f135001ac5e97946
metalboyrick <rsulisthio@gmail.com>
2025-08-04 19:36:59 +0700

    fix: type-safe for getContractByName

--- extensions/packages/auco/src/utils/contracts.ts
+++ extensions/packages/auco/src/utils/contracts.ts
@@ -1,6 +1,14 @@
 import deployedContracts from "../contracts/deployedContracts";
 import globalConfig from "../../auco.config";
 
+// Dynamically extract types from the actual deployedContracts structure
+type AvailableNetworks = keyof typeof deployedContracts;
+type CurrentNetwork = typeof globalConfig.network extends AvailableNetworks
+  ? typeof globalConfig.network
+  : AvailableNetworks;
+type CurrentNetworkContracts = (typeof deployedContracts)[CurrentNetwork];
+type AvailableContracts = keyof CurrentNetworkContracts;
+
 /**
  * Retrieves contract information by name from the deployed contracts configuration
  * for the currently configured network. To be used with auco onEvent or onReorg
@@ -8,12 +16,21 @@ import globalConfig from "../../auco.config";
  * @param name - The name of the contract to retrieve, must be a valid contract name
  *               for the current network configuration
  * @returns An object containing the contract address and ABI for the specified contract
+ *
+ * @example
+ * ```typescript
+ * const { contractAddress, abi } = getContractByName("MyContract");
+ * ```
  */
-export const getContractByName = (
-  name: keyof (typeof deployedContracts)[keyof typeof deployedContracts],
-) => {
+export const getContractByName = <T extends AvailableContracts>(
+  name: T,
+): {
+  contractAddress: CurrentNetworkContracts[T]["address"];
+  abi: CurrentNetworkContracts[T]["abi"];
+} => {
   return {
-    contractAddress: deployedContracts[globalConfig.network][name].address,
-    abi: deployedContracts[globalConfig.network][name].abi,
+    contractAddress:
+      deployedContracts[globalConfig.network as CurrentNetwork][name].address,
+    abi: deployedContracts[globalConfig.network as CurrentNetwork][name].abi,
   };
 };
