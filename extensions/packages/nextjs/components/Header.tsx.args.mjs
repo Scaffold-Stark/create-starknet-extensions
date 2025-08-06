1562b741949f43dd37799b25934afd15d5b6cc2a
Richard (Rick) <53423618+metalboyrick@users.noreply.github.com>
2025-08-06 23:17:05 +0700

    feat: update header template (#2)

--- extensions/packages/nextjs/components/Header.tsx.args.mjs
+++
@@ -1,8 +0,0 @@
-export const preContent = `import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";`;
-export const extraMenuLinksObjects = [
-  {
-    label: "Auco",
-    href: "/auco-greetings",
-    icon: '$$<MagnifyingGlassIcon className="h-4 w-4" />$$',
-  },
-];
