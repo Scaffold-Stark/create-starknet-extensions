0664e71abd34c3fce6241e7ea04838afd8ec8b17
tu11aa <thanhtuhr01@gmail.com>
2025-10-04 13:06:22 +0700

    test

--- extensions/packages/nextjs/app/erc-20/page.tsx
+++ extensions/packages/nextjs/app/erc-20/page.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from "next";
 const ERC20Page: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-5xl">
-      This is a test
+      This is a test for CI
     </div>
   );
 };
